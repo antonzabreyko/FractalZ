f718a66a4a637bdb672855f0ef711f98e8aecd88
antonzabreyko <anton.zabreyko@gmail.com>
2020-05-25 23:50:48 -0700

    Began implementing the algorithm

--- Fractal.py
+++ Fractal.py
@@ -1,4 +1,35 @@
+import io
 import csv
+import numpy as np
 
-def generateFractals(file, level, curr_level=0):
+'''Given the input file and the number of additional levels to be generated,
+    create a new file called output.csv with the new data. '''
+def generateFractals(file, level, scale_factor, center=None):
+    data = readFile(file)
+
+    newData = fractalAlgorithm(data, scale_factor, center)
+
+    createCSV(newData)
+
+
+def fractalAlgorithm(data, s, c):
+    N = data.shape[0]
+    D = data.shape[1]
+
+    A = np.zeros((N*N, N))
+
+    for i in range(N):
+        for j in range(N):
+            A[]
+
+
+
+
+def readFile(file):
+    return
+
+def fractalAlgorithm(data, center):
+    return
+
+def createCSV(newData):
     return

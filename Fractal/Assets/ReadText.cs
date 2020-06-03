fef4c74694334eabfbbb58fa35016b8fd7dbf1a2
Seung Woon Lee <swl4@illinois.edu>
2020-06-03 02:44:46 -0700

    made first attempt to read, put unity in git

---
+++ Fractal/Assets/ReadText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class ReadText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        string path = "Assets/Resources/test";
+
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+        Debug.Log(reader.ReadToEnd());
+        reader.Close();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

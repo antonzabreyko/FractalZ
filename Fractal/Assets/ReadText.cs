1708a94c5bc175b4445b99542fe2401f91128264
Seung Woon Lee <swl4@illinois.edu>
2020-06-12 00:50:19 -0700

    almost there. Need to flesh it out, but have line renderer and the text to numbers

--- Fractal/Assets/ReadText.cs
+++ Fractal/Assets/ReadText.cs
@@ -17,18 +17,34 @@ public class ReadText : MonoBehaviour
         while (!reader.EndOfStream)
         {
             strings.Add(reader.ReadLine());
-            
         }
         reader.Close();
         //metadata
         Debug.Log(strings[0]);
         //points
+        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.widthMultiplier = 0.2f;
+        lineRenderer.positionCount = 4;
+
+        float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+        );
+        lineRenderer.colorGradient =gradient;
+        var points = new Vector3[4];
+
+
         for (int i = 1; i < strings.Count; i++)
         {
             //Debug.Log(strings[i]);
             string[] coordinates = strings[i].Split(',');
-            int x = Int32.parse(coordinates[0]);
-            int y = Int32.parse(coordinates[1]);
+            float x = float.Parse(coordinates[0]);
+            float y = float.Parse(coordinates[1]);
+            points[i] = new Vector3(x, y, 0.0f);
+            lineRenderer.SetPositions(points);
         }
     }
 

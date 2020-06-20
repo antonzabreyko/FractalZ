08a068d388071a3500951fcca0b790c040393c36
Seung Woon Lee <swl4@illinois.edu>
2020-06-19 21:18:18 -0700

    version 1 done! Debugged!

--- Fractal/Assets/ReadText.cs
+++ Fractal/Assets/ReadText.cs
@@ -22,18 +22,18 @@ public class ReadText : MonoBehaviour
         //metadata
         Debug.Log(strings[0]);
         //points
-        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
-        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-        lineRenderer.widthMultiplier = 0.2f;
-        lineRenderer.positionCount = 4;
+
+        gameObject.GetComponent<LineRenderer>().material = new Material(Shader.Find("Sprites/Default"));
+        gameObject.GetComponent<LineRenderer>().widthMultiplier = 0.2f;
+        gameObject.GetComponent<LineRenderer>().positionCount = 4;
 
         float alpha = 1.0f;
         Gradient gradient = new Gradient();
         gradient.SetKeys(
-            new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
+            new GradientColorKey[] { new GradientColorKey(Color.black, 0.0f), new GradientColorKey(Color.blue, 1.0f) },
             new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
         );
-        lineRenderer.colorGradient =gradient;
+        gameObject.GetComponent<LineRenderer>().colorGradient =gradient;
         var points = new Vector3[4];
 
 
@@ -44,7 +44,7 @@ public class ReadText : MonoBehaviour
             float x = float.Parse(coordinates[0]);
             float y = float.Parse(coordinates[1]);
             points[i] = new Vector3(x, y, 0.0f);
-            lineRenderer.SetPositions(points);
+            gameObject.GetComponent<LineRenderer>().SetPositions(points);
         }
     }
 
